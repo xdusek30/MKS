450ef6fdf8987b0fb745f13b5b47a54630ce0297
Filip Dusek <211545@vut.cz>
2021-09-23 11:50:40 +0200

    morseovka cast 1

--- cv1/Src/main.c
+++ cv1/Src/main.c
@@ -18,6 +18,7 @@
  */
 
 #include <stdint.h>
+#include "stm32f0xx.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -25,6 +26,29 @@
 
 int main(void)
 {
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+	 uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
+
+
     /* Loop forever */
-	for(;;);
+	for(;;){
+
+		for(uint32_t i=0; i<32; i++){
+			if (pole[i]==1){
+
+				GPIOA->BSRR = (1<<5); // set
+			}
+
+			else{
+
+				GPIOA->BRR = (1<<5); // reset
+			}
+
+			for (volatile uint32_t i = 0; i < 100000; i++) {}
+		}
+
+
+	}
+
 }
