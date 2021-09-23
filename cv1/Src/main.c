6ae67a6101d3d0ba442fff403ce214688653c34c
Filip Dusek <211545@vut.cz>
2021-09-23 12:17:23 +0200

    druha cast morseovka

--- cv1/Src/main.c
+++ cv1/Src/main.c
@@ -28,12 +28,13 @@ int main(void)
 {
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0;
-	 uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
+	/* uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0}; */
+
 
 
     /* Loop forever */
 	for(;;){
-
+	/*
 		for(uint32_t i=0; i<32; i++){
 			if (pole[i]==1){
 
@@ -47,8 +48,24 @@ int main(void)
 
 			for (volatile uint32_t i = 0; i < 100000; i++) {}
 		}
+	*/
+		uint32_t pole = 0b10101001110111011100101010000000;
 
+		for(uint8_t i=0; i<32; i++){
+			if (pole & 1){
+
+				GPIOA->BSRR = (1<<5); // set
+			}
+
+			else{
+
+				GPIOA->BRR = (1<<5); // reset
+			}
+
+			pole = pole >> 1;
+
+			for (volatile uint32_t i = 0; i < 100000; i++) {}
+		}
 
 	}
-
 }
